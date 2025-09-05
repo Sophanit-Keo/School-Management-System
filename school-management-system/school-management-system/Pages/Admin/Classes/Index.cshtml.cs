50f9357fb65d3b77621e29e57107507a0d023b2f
Sophanit-Keo <sophanitkeo1803@gmail.com>
2025-09-05 16:15:25 +0700

    add the admin dashbord template to ASP project

---
+++ school-management-system/school-management-system/Pages/Admin/Classes/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace school_management_system.Pages.Admin.Classes
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ school-management-system/school-management-system/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace school_management_system.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
